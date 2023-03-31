a97141424131ff57448ed78f041049df8811b294
xccado <100438949+xccado@users.noreply.github.com>
2023-03-31 08:52:59 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,12 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'IMSYY 站点监测',
+  SiteName: 'qljdv 站点监测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'u1022423-6ffcda436b183008644c691c',
+    'u2073446-0517227ec3d491a16d1ae739',
   ],
 
   // 日志天数
@@ -19,15 +19,15 @@ window.Config = {
   Navi: [
     {
       text: '主页',
-      url: 'https://www.imsyy.top/'
+      url: 'https://www.qljdv.top/'
     },
     {
       text: '博客',
-      url: 'https://blog.imsyy.top/'
+      url: 'https://blog.qljdv.top/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/imsyy/'
+      url: 'https://github.com/xccado/'
     },
   ],
 };
